438d2dd1993eb54a53c8488b32b8282b66792095
sy1100 <sierrayang12@gmail.com>
2026-02-23 00:55:14 -0600

    Inventory folder
    This is frontend for inventory folders
    - Inventory folder view
    1. Add categories
    2. Add Item
    3. ︙icon for reassign category (function was not implemented) 
    -Edit mode
    1. Delete Category
    2. Delete Item
    3. Edit Category - Long press
    4. Edit Item name

--- LoopedIn/app/_layout.tsx
+++ LoopedIn/app/_layout.tsx
@@ -33,6 +33,7 @@ export default function RootLayout() {
         <Stack.Screen name="trackerFolder/[id]" options={{ headerShown: false, animation: "none" }} />
         <Stack.Screen name="singleProject/[id]" options={{ headerShown: false, animation: "none" }} />
         <Stack.Screen name="newproject" options={{ headerShown: false, animation: "none" }} />
+        <Stack.Screen name="inventoryfolder" options={{ headerShown: false, animation: "none" }} />
       </Stack>
     </ThemeProvider>
   );
