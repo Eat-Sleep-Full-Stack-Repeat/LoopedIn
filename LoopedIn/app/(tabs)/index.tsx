6cd47816b13610c91af389aded2c79d701576d3b
madisontru908 <madisontru908@gmail.com>
2026-03-03 18:09:54 -0600

    Clean up index (remove routes)

--- LoopedIn/app/(tabs)/index.tsx
+++ LoopedIn/app/(tabs)/index.tsx
@@ -48,12 +48,6 @@ export default function Index() {
         <Text style={{ color: colors.text }}>Welcome Page</Text>
       </TouchableOpacity>
       <TouchableOpacity
-        onPress={() => router.push("/folderscreen")}
-        style={{ marginTop: 20 }}
-      >
-        <Text style={{ color: colors.text }}> My Folders </Text>
-      </TouchableOpacity>
-      <TouchableOpacity
         onPress={() => router.push("/inventoryfolder")}
         style={{ marginTop: 20 }}
       >
@@ -70,13 +64,6 @@ export default function Index() {
         style={{ marginTop: 20 }}
       >
         <Text style={{ color: colors.text }}> New Project </Text>
-      </TouchableOpacity>
-
-      <TouchableOpacity
-        onPress={() => router.push("/mystuff")}
-        style={{ marginTop: 20 }}
-      >
-        <Text style={{ color: colors.text }}> My Stuff </Text>
       </TouchableOpacity>
 
       <View
