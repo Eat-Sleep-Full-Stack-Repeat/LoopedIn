65312341a5b2e1775bbf403926287ef219a29eac
Emily Warren <emilywarren056@gmail.com>
2026-03-25 15:57:07 -0500

    backend changes (mostly delete/post endpoints)

--- backend/api/post.js
+++ backend/api/post.js
@@ -13,6 +13,34 @@ const { generateColor } = require("../functions/color_generator");
 
 require("dotenv").config();
 
+const deleteFile = async (fileName, folderName) => {
+  try {
+    const key = `${folderName}/${fileName}`;
+
+    const params = {
+      Bucket: process.env.BUCKET_NAME,
+      Key: key,
+    };
+
+    const command = new DeleteObjectCommand(params);
+    await s3.send(command);
+
+    console.log(`[post]: Deleted from S3 -> ${key}`);
+  } catch (err) {
+    console.error("[post]: Error deleting file from S3:", err);
+  }
+};
+
+//initialize S3
+const { S3Client, DeleteObjectCommand } = require("@aws-sdk/client-s3");
+const s3 = new S3Client({
+    region: process.env.BUCKET_REGION,
+    credentials: {
+    accessKeyId: process.env.BUCKET_ACCESS_KEY,
+    secretAccessKey: process.env.BUCKET_SECRET_KEY,
+  },
+});
+
 /**
  * Multer setup: memory storage, 5MB max per file
  */
@@ -906,12 +934,39 @@ router.delete("/delete-post", authenticateToken, async (req, res) => {
   const postToDelete = req.body.PostID;
 
   try {
+
+    //get image info to delete from S3
+    queryPics = `
+    SELECT fld_pic_id, fld_post_pic
+    FROM posts.tbl_post_pic
+    WHERE fld_post_fk = $1 AND fld_pic_id IS NOT NULL;
+    `
+
+    let image_key = await pool.query(queryPics, [postToDelete]);
+
     const query = `
       DELETE FROM posts.tbl_post
       WHERE fld_post_pk = $1 AND fld_creator = $2;
     `;
 
     await pool.query(query, [postToDelete, user]);
+
+    //now S3 image deletion time
+    if (image_key.rowCount != 0) {
+      for(let i = 0; i < image_key.rowCount; i++){
+        image_key = image_key.rows[i].fld_post_pic
+
+        //get variables
+        const fullKey = image_key.includes("/") ? image_key : `posts/${image_key}`;
+        const folderName = fullKey.split("/")[0];
+        const fileName = fullKey.split("/").slice(1).join("/");
+
+        //console.log("[posts]: delete post images in process. foldername, filename ", folderName, fileName)
+
+        //delete images (hopefully)
+        await deleteFile(fileName, folderName);
+      }
+    }
 
     res.status(200).json({ message: "Successfully deleted the post!" });
   } catch (e) {
